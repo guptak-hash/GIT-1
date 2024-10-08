0288620f92fff3f052471bdcc047411705202d80
guptak-hash <guptakishan1123@gmail.com>
2024-10-08 23:20:24 +0530

    filter even number

--- Problem_2.js
+++ Problem_2.js
@@ -1,2 +1,16 @@
 // use of filter()
 
+let input = [1,2,3,4,5,6];
+
+let output = input.filter(fn);
+
+function fn(e){
+    if(e%2===0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+console.log(output);
